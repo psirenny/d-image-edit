ddbebac104bc48e5b56fb1df16a79cae03acd6c6
psirenny <djtorres0@gmail.com>
2014-02-03 12:49:38 -0500

    naming, scale image

--- upload/util/image.js
+++ upload/util/image.js
@@ -19,7 +19,7 @@ exports.create = function (data, callback) {
 
 exports.resize = function (image, width, height, callback) {
   var canvas = document.createElement('canvas')
-    , context = canvas.getContext('2d')
+    , ctx = canvas.getContext('2d')
     , resizedImage = new Image();
 
   if (image.width === width && image.height === height) {
@@ -32,37 +32,41 @@ exports.resize = function (image, width, height, callback) {
 
   canvas.width = width;
   canvas.height = height;
-  context.drawImage(image, 0, 0, width, height);
+  ctx.drawImage(image, 0, 0, width, height);
   resizedImage.src = canvas.toDataURL('image/png');
 };
 
 exports.toBlob = function (image, callback) {
   var canvas = document.createElement('canvas')
-    , context = canvas.getContext('2d');
+    , ctx = canvas.getContext('2d');
 
   canvas.width = image.width;
   canvas.height = image.height;
-  context.drawImage(image, 0, 0);
+  ctx.drawImage(image, 0, 0);
   canvas.toBlob(function (blob) {
     callback(null, blob);
   });
 };
 
-exports.transform = function (image, matrix, containerWidth, containerHeight, callback) {
+exports.transform = function (image, matrix, canvasWidth, canvasHeight, callback) {
   var canvas = document.createElement('canvas')
-    , context = canvas.getContext('2d')
-    , x = (image.width - image.width * matrix[0]) / 2
-    , y = (image.height - image.height * matrix[3]) / 2
+    , ctx = canvas.getContext('2d')
+    , x1 = (image.width - (image.width * matrix[0])) / 2
+    , x2 = matrix[4] + x1
+    , y1 = (image.height - (image.height * matrix[3])) / 2
+    , y2 = matrix[5] + y1
     , transformedImage = new Image();
 
   transformedImage.onload = function () {
     callback(null, transformedImage);
   };
 
-  canvas.width = containerWidth;
-  canvas.height = containerHeight;
-  context.translate(x, y);
-  context.transform.apply(context, matrix);
-  context.drawImage(image, 0, 0);
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  ctx.translate(x1, y1);
+  ctx.transform.apply(ctx, matrix);
+  ctx.scale(2, 2);
+  ctx.translate(x2, y2);
+  ctx.drawImage(image, 0, 0);
   transformedImage.src = canvas.toDataURL('image/png');
 };
